16082aabb5b0bd7be63d4c8193988ab1c42046df
sabrina-fontana <sabfon@hotmail.it>
2021-01-28 14:14:49 +0100

    inizio esercizio

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+el: '#root',
+data: {
+  input: '',
+  arrayInput: []
+},
+methods: {
+  submit: function() {
+    if (this.input.length !== 0 && this.input !== '') {
+      this.arrayInput.push(this.input);
+      this.input= '';
+    }
+  },
+  clickFunction: function(htmlIndex) {
+    // filtro l'array input resistuendo solo gli elementi che hanno indice diverso dall'elemento clickato
+    this.arrayInput = this.arrayInput.filter((element, index) => {
+      if (htmlIndex !== index) {
+        return element
+      }
+    });
+  }
+}
+});
+Vue.config.devtools = true;
